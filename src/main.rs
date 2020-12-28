bf6c8a1f32b9dd47df581f1716a06c22416bf473
Renjith <grenjithr@gmail.com>
2020-12-28 02:17:37 -0600

    Muliple PRINT statements working

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,11 @@ use crate::run::run;
 
 fn main() {
     let input = "
-        PRINT \"HELLO WORLD\"
+        PRINT \"H         D\"
+        PRINT \" E       L \"
+        PRINT \"  L     R  \"
+        PRINT \"   L   O   \"
+        PRINT \"    O W    \"
     ";
     let tokens = get_tokens(input);
     let program = parse_tokens(tokens);
